bb284067b132693fba9da47ab7f07f86ae936e71
Cortlin <cortlin.sawyer@gmail.com>
2021-02-25 10:34:09 -0600

    nav tweaks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,13 @@ export const Nav = () => {
     <nav className='px-6 shadow-lg'>
       <div className='max-w-md mx-auto flex'>
         <ul className='text-gray-800 flex flex-1 justify-center first:mr-auto last:ml-auto md:text-lg'>
-          <li className='py-4 hover:underline px-10'>
+          <li className='py-4 px-5 hover:underline md:px-10'>
             <Link to='/'>About</Link>
           </li>
-          <li className='py-4 hover:underline px-10'>
+          <li className='py-4 px-5 hover:underline md:px-10'>
             <Link to='/portfolio'>Portfolio</Link>
           </li>
-          <li className='py-4 hover:underline px-10'>
+          <li className='py-4 px-5 hover:underline md:px-10'>
             <Link className=' whitespace-nowrap' to='/experience'>Work Experience</Link>
           </li>
         </ul>
