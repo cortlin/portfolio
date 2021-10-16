e81a419e67555a48860fa4b1981f78e95f7180db
Cortlin Sawyer <cortlinsawyer@Cortlinsawyers-MacBook-Pro.local>
2021-10-16 11:56:50 -0700

    updated email address

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,7 +24,7 @@ export const About = () => {
               </svg>
             </a>
 
-            <a href="mailto:cortlin@hey.com">
+            <a href="mailto:cortlin.sawyer@gmail.com">
               <svg className='mx-4 w-10 h-10 hover:text-blue-600 transition transform duration-500 hover:-translate-y-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
